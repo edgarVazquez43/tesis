fc6f214f33cc079419ee8b07d131d8dfd87292a1
edgarVazquez43 <edgar_vazquez43@hotmail.com>
2016-12-02 13:02:17 -0600

    Creación de la carpeta Kinect para desarrollo de código en C++. Para el procesamiento de imagen con Kinect.

---
+++ kinect_codes/cpp_opencv/kinect_00/CMakeFiles/kinect00.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/kinect00.dir/kinect00.cpp.o"
+  "kinect00.pdb"
+  "kinect00"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/kinect00.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ kinect_codes/cpp_opencv/prueba_1/CMakeFiles/prueba_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/prueba_1.dir/prueba_1.cpp.o"
+  "prueba_1.pdb"
+  "prueba_1"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/prueba_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
