44bfc5b81f5a98a053e010efc70bda92528cb675
edgarVazquez43 <edgar_vazquez43@hotmail.com>
2017-01-18 18:42:59 -0600

    Quitando las carpetas build y devel

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/edgar-ii/github/tesis/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/edgar-ii/github/tesis/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/edgar-ii/github/tesis/catkin_ws/devel;/opt/ros/indigo" != "" else []
--- catkin_ws/build/kinect_man/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/edgar-ii/github/tesis/catkin_ws/src/kinect_man
-# Build directory: /home/edgar-ii/github/tesis/catkin_ws/build/kinect_man
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
