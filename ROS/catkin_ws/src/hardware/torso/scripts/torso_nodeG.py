3c17070861cee434e568a01c0ac7672486765b29
edgarVazquez43 <edgarvazquez1403@gmail.com>
2017-12-18 15:23:20 -0600

    Add torso node and torso to xml_description.

--- ROS/catkin_ws/src/hardware/torso/scripts/torso_nodeG.py
+++ ROS/catkin_ws/src/hardware/torso/scripts/torso_nodeG.py
@@ -2,12 +2,15 @@
 import sys
 import rospy
 import math
+import time
 from std_msgs.msg import Float32MultiArray
 from std_msgs.msg import Float32
 from std_msgs.msg import Bool
 from geometry_msgs.msg import TransformStamped
 from sensor_msgs.msg import JointState
 import tf
+import torso_driver as Torso
+torso = None
 
 def printHelp():
     print "YOU DON'T REALLY NEED HELP TO OPERATE TORSO IN SIMULATION MODE"
@@ -24,9 +27,34 @@ def callbackGoalPose(msg):
     goalWaist = msg.data[1]
     goalShoulders = msg.data[2]
     newGoal = True;
-    msg = Bool()
-    msg.data = False
-    pubGoalReached.publish(msg)
+    msgGoalReached = Bool()
+    msgGoalReached.data = False
+    pubGoalReached.publish(msgGoalReached)
+
+
+    dato = msg.data[0]*100
+    if dato>45:
+        dato=45
+    if dato<0:
+        dato=0
+    torso.columna[0] = int(dato)
+
+    dato = msg.data[1]*180.0/3.1416
+    if dato>90:
+        dato=90
+    if dato<-90:
+        dato=-90
+    torso.torso[0] = int(dato)
+
+    dato = msg.data[2]*180.0/3.1416
+    if dato>20:
+        dato=20
+    if dato<-20:
+        dato=-20
+    torso.hombro[0] = int(dato)
+
+
+
 
 def callbackRelPose(msg):
     if len(msg.data) != 3:
@@ -43,13 +71,42 @@ def callbackRelPose(msg):
     goalWaist = waist + msg.data[1]
     goalShoulders = shoulders + msg.data[2]
     newGoal = True
-    msg = Bool()
-    msg.data = False
-    pubGoalReached.publish(msg)
+    msgGoalReached = Bool()
+    msgGoalReached.data = False
+    pubGoalReached.publish(msgGoalReached)
+
+    dato = goalSpine*100
+    if dato>45:
+        dato=45
+    if dato<0:
+        dato=0
+    torso.columna[0] = int(dato)
+
+    dato = goalWaist*180.0/3.1416
+    if dato>90:
+        dato=90
+    if dato<-90:
+        dato=-90
+    torso.torso[0] = int(dato)
+
+    dato = goalShoulders*180.0/3.1416
+    if dato>20:
+        dato=20
+    if dato<-20:
+        dato=-20
+    torso.hombro[0] = int(dato)
         
 
-def main():
-    print "INITIALIZING TORSO NODE IN SIMULATION MODE BY MARCOSOFT..."
+def main(portName):
+    print "INITIALIZING TORSO NODE BY MARCOSOFT..."
+    
+    #init torso
+    global torso
+    torso=Torso.Torso(False)
+    torso.SetSerial(portName,115200)
+    
+    #torso.start()
+
     ###Connection with ROS
     global pubGoalReached
     rospy.init_node("torso")
@@ -73,13 +130,12 @@ def main():
     global waist
     global shoulders
     global newGoal
-    spine = 0.10
+    goalSpine = 0.0
+    goalWaist = 0.0
+    goalShoulders = 0.0
+    spine = 0
     waist = 0
     shoulders = 0
-    goalSpine = spine
-    goalWaist = waist
-    goalShoulders = shoulders
-    
     speedSpine = 0.005
     speedWaist = 0.1
     speedShoulders = 0.1
@@ -88,26 +144,26 @@ def main():
     msgCurrentPose.data = [0, 0, 0]
     newGoal = False
     
-    while not rospy.is_shutdown():
-        deltaSpine = goalSpine - spine;
-        deltaWaist = goalWaist - waist;
-        deltaShoulders = goalShoulders - shoulders;
-        if deltaSpine > speedSpine:
-            deltaSpine = speedSpine;
-        if deltaSpine < -speedSpine:
-            deltaSpine = -speedSpine;
-        if deltaWaist > speedWaist:
-            deltaWaist = speedWaist;
-        if deltaWaist < -speedWaist:
-            deltaWaist = -speedWaist;
-        if deltaShoulders > speedShoulders:
-            deltaShoulders = speedShoulders;
-        if deltaShoulders < -speedShoulders:
-            deltaShoulders = -speedShoulders;
+    torso.ComBase()#linea agregada para comunicar con la base
+    
+    time.sleep(1)
+    torso.columna[0]=torso.columna[1]
+    torso.columna[2]=True
+    torso.torso[2]=False
+    torso.hombro[2]=False
 
-        spine += deltaSpine
-        waist += deltaWaist
-        shoulders += deltaShoulders
+    while not rospy.is_shutdown():
+
+        torso.ComBase()#linea agregada para comunicar con la base
+
+
+        spine=torso.columna[1]/100.0 - 0.2
+        waist=0#torso.torso[1]*3.1416/180.0
+        shoulders=0#torso.hombro[1]*3.1416/180.0
+
+
+
+
         jointStates.header.stamp = rospy.Time.now()
         jointStates.position = [spine, waist, shoulders, -shoulders, -shoulders]
         pubJointStates.publish(jointStates)
@@ -117,20 +173,28 @@ def main():
         msgCurrentPose.data[2] = shoulders
         pubCurrentPose.publish(msgCurrentPose)
 
-        if newGoal and abs(goalSpine - spine) < 0.02 and abs(goalWaist - waist) < 0.05 and abs(goalShoulders - shoulders) < 0.05:
+        if newGoal and abs(goalSpine - spine) < 0.015 and abs(goalWaist - waist) < 0.05 and abs(goalShoulders - shoulders) < 0.05:
             newGoal = False
             msgGoalReached.data = True
             pubGoalReached.publish(msgGoalReached)
         
         loop.sleep()
+    
+    torso.columna[2]=False
+    torso.torso[2]=False
+    torso.hombro[2]=False
+    torso.runBase = False
 
 if __name__ == '__main__':
     try:
+        portName = "/dev/ttyACM2"
         if "--help" in sys.argv:
             printHelp()
         elif "-h" in sys.argv:
             printHelp()
-        else:
-            main()
+        elif "--port" in sys.argv:
+            portName = sys.argv[sys.argv.index("--port") + 1]
+            main(portName)
+
     except rospy.ROSInterruptException:
         pass
