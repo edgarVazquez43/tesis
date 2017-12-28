3eadb4df5dbdb46e72ae401b47524068362e4d59
edgarVazquez43 <edgarvazquez1403@gmail.com>
2017-12-27 19:02:23 -0600

    Corrections for duplicate files pdf.

--- latex/src/latex.py
+++ latex/src/latex.py
@@ -2,11 +2,13 @@
 
 import os, sys
 
-commands = ["pdflatex " + sys.argv[1] + ".tex",
+commands = [
+    "pdflatex " + sys.argv[1] + ".tex",
     "bibtex " + sys.argv[1] + ".aux",
     "pdflatex " + sys.argv[1] + ".tex",
     "pdflatex " + sys.argv[1] + ".tex",
-    "mv " + sys.argv[1] + ".pdf ../pdf"
+    "mv " + sys.argv[1] + ".pdf ../pdf",
+    "cp ../pdf/" + sys.argv[1] + ".pdf ../pdf/Desarrollo_de_un_sistema_de_deteccion_y_manipulacion_de_objetos.pdf"
 ]
 
 for c in commands:
