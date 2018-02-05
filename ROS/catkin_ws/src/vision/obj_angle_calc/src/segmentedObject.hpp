f6799d1b24764a4206cfc8743fd6547004625d53
edgarVazquez <edgarvazquez1403@gmail.com>
2018-02-05 16:54:35 -0600

    Improvements into pca calculator.

--- ROS/catkin_ws/src/vision/obj_angle_calc/src/segmentedObject.hpp
+++ ROS/catkin_ws/src/vision/obj_angle_calc/src/segmentedObject.hpp
@@ -7,20 +7,21 @@
 class segmentedObject{
   
 public:
-	segmentedObject();
-	segmentedObject(cv::Mat pointsBRG, cv::Mat pointsObject, int xMin, int yMin, int xMax, int yMax);
+  segmentedObject();
+  segmentedObject(cv::Mat pointsBRG, cv::Mat pointsObject, int xMin, int yMin, int xMax, int yMax);
 
-	bool getCentroid();
-	bool getPrincipalAxis();
+  bool getCentroid();
+  bool getPrincipalAxis();
 
-	cv::Mat pointsObject;
-	cv::Mat pointsBRG;
-  	cv::Rect ROI;
-  	std::vector<float> centroid;
-	std::vector<cv::Point3f> principalAxis;
-	std::vector<float> dimensions;
-	std::vector<float> colors;
-	std::vector<float> standartDeviations;
+  cv::Mat pointsObject;
+  cv::Mat pointsBRG;
+  cv::Rect ROI;
+  std::vector<float> centroid;
+  std::vector<cv::Point3f> principalAxis;
+  std::vector<float> colors;
+  std::vector<float> dimensions;
+
+  std::vector<float> standartDeviations;
 
 private:
 
@@ -33,134 +34,135 @@ segmentedObject::segmentedObject()
 }
 
 
-// Definicion de un plano por tres puntos
 segmentedObject::segmentedObject(cv::Mat pointsBRG, cv::Mat pointsObject, int xMin, int yMin, int xMax, int yMax)
 {
-	cv::Rect myCrop(xMin, yMin, xMax - xMin, yMax - yMin);
-	this-> pointsBRG = pointsBRG;
-	this-> pointsObject = pointsObject;
-	this-> ROI = myCrop;
+  cv::Rect myCrop(xMin, yMin, xMax - xMin, yMax - yMin);
+  this-> pointsBRG = pointsBRG;
+  this-> pointsObject = pointsObject;
+  this-> ROI = myCrop;
 }
 
 bool segmentedObject::getCentroid()
 {
-	int points_obj;
-	float x_obj;
-	float y_obj;
-	float z_obj;
-	cv::Point3f px;
+  int points_obj;
+  float x_obj;
+  float y_obj;
+  float z_obj;
+  cv::Point3f px;
 
-	x_obj = 0.0;
-	y_obj = 0.0;
-	z_obj = 0.0;
+  x_obj = 0.0;
+  y_obj = 0.0;
+  z_obj = 0.0;
 
-	points_obj = 0;
+  points_obj = 0;
 
-	// Search the centroid of object PointCloud
-	for(int j = 0; j < this->pointsObject.rows; j++)
-		for (int i = 0; i < this->pointsObject.cols; i++)
-		{
-			px = this->pointsObject.at<cv::Point3f>(j,i);
-			if ( px != cv::Point3f(0.0, 0.0, 0.0) && px != cv::Point3f(0, 255, 0))
-			{
-				x_obj += px.x;
-				y_obj += px.y;
-				z_obj += px.z;
-				points_obj++;
-				//std::cout << "x_obj: " << px.x << " - y_obj: " << px.y << " - z_obj: " << px.z << std::endl;
-			}
-		}
+  // Search the centroid of object PointCloud
+  for(int j = 0; j < this->pointsObject.rows; j++)
+    for (int i = 0; i < this->pointsObject.cols; i++)
+      {
+	px = this->pointsObject.at<cv::Point3f>(j,i);
+	if ( px != cv::Point3f(0.0, 0.0, 0.0) && px != cv::Point3f(0, 255, 0))
+	  {
+	    x_obj += px.x;
+	    y_obj += px.y;
+	    z_obj += px.z;
+	    points_obj++;
+	    //std::cout << "x_obj: " << px.x << " - y_obj: " << px.y << " - z_obj: " << px.z << std::endl;
+	  }
+      }
 
-	x_obj = x_obj/points_obj;
-	this->centroid.push_back(x_obj);
+  x_obj = x_obj/points_obj;
+  this->centroid.push_back(x_obj);
 
-	y_obj = y_obj/points_obj;
-	this->centroid.push_back(y_obj);
+  y_obj = y_obj/points_obj;
+  this->centroid.push_back(y_obj);
 
-	z_obj = (z_obj/points_obj) ;
-	this->centroid.push_back(z_obj);
+  z_obj = (z_obj/points_obj) ;
+  this->centroid.push_back(z_obj);
 
-	return true;
+  return true;
 
 }
 
 bool segmentedObject::getPrincipalAxis()
 {
-	int n;
-	float var_x;
-	float var_y;
-	float var_z;
-	float cov_xy;
-	float cov_xz;
-	float cov_yz;
-	std::vector<cv::Point3f> principal_comp;
+  int n;
+  float var_x;
+  float var_y;
+  float var_z;
+  float cov_xy;
+  float cov_xz;
+  float cov_yz;
+  std::vector<cv::Point3f> principal_comp;
 
-	cv::Point3f px;
-	cv::Point3f axis_1, axis_2, axis_3;
+  cv::Point3f px;
+  cv::Point3f axis_1, axis_2, axis_3;
 
-	Eigen::Matrix3f cov_matrix(3,3);
-	//Eigen::Vector3f eivals;
+  Eigen::Matrix3f cov_matrix(3,3);
+  //Eigen::Vector3f eivals;
 
-	n = 0;
-	var_x = 0.0;
-	var_y = 0.0;
-	var_z = 0.0;
-	cov_xy = 0.0;
-	cov_xz = 0.0;
-	cov_yz = 0.0;
+  n = 0;
+  var_x = 0.0;
+  var_y = 0.0;
+  var_z = 0.0;
+  cov_xy = 0.0;
+  cov_xz = 0.0;
+  cov_yz = 0.0;
 
-	//Calculate covariance matrix
-	for(int j = 0; j < this->pointsObject.rows; j++)
-		for (int i = 0; i < this->pointsObject.cols; i++)
-		{
-			px = this->pointsObject.at<cv::Point3f>(j,i);
-			if ( px != cv::Point3f(0.0, 0.0, 0.0) && px != cv::Point3f(0, 255, 0))
-			{
-				var_x += pow( (px.x - this->centroid[0]), 2 );
-				var_y += pow( (px.y - this->centroid[1]), 2 );
-				var_z += pow( (px.z - this->centroid[2]), 2 );
-				cov_xy += ( px.x - this->centroid[0] )*( px.y - this->centroid[1] );
-				cov_xz += ( px.x - this->centroid[0] )*( px.z - this->centroid[2] );
-				cov_yz += ( px.y - this->centroid[1] )*( px.z - this->centroid[2] );
-				n++;
-			}
-		}
+  //Calculate covariance matrix
+  for(int j = 0; j < this->pointsObject.rows; j++)
+    for (int i = 0; i < this->pointsObject.cols; i++)
+      {
+	px = this->pointsObject.at<cv::Point3f>(j,i);
+	if ( px != cv::Point3f(0.0, 0.0, 0.0) && px != cv::Point3f(0, 255, 0))
+	  {
+	    var_x += pow( (px.x - this->centroid[0]), 2 );
+	    var_y += pow( (px.y - this->centroid[1]), 2 );
+	    var_z += pow( (px.z - this->centroid[2]), 2 );
+	    cov_xy += ( px.x - this->centroid[0] )*( px.y - this->centroid[1] );
+	    cov_xz += ( px.x - this->centroid[0] )*( px.z - this->centroid[2] );
+	    cov_yz += ( px.y - this->centroid[1] )*( px.z - this->centroid[2] );
+	    n++;
+	  }
+      }
 
-	var_x /= n;
-	var_y /= n;
-	var_z /= n;
+  var_x /= n;
+  var_y /= n;
+  var_z /= n;
 
-	cov_xy /= n;
-	cov_xz /= n;
-	cov_yz /= n;
+  cov_xy /= n;
+  cov_xz /= n;
+  cov_yz /= n;
 
-	cov_matrix << var_x,  cov_xy, cov_xz,
-				 cov_xy,  var_y, cov_yz,
-				 cov_xz, cov_yz,  var_z;
+  // Fill Covariance Matrix
+  cov_matrix << var_x,  cov_xy, cov_xz,
+    cov_xy,  var_y, cov_yz,
+    cov_xz, cov_yz,  var_z;
+  //std::cout << "    cov_matrix: " << std::endl << cov_matrix << std::endl;
 
-	//std::cout << "    cov_matrix: " << std::endl << cov_matrix << std::endl;
+  
+  // Calculate eigenvalues and eigenvectors of covariance matrix
+  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXf> eig(cov_matrix);
 
-	Eigen::SelfAdjointEigenSolver<Eigen::MatrixXf> eig(cov_matrix);
+  //std::cout << "    eigenvectors: " << std::endl << eig.eigenvectors().col(0) <<
+  //			std::endl << eig.eigenvectors().col(1) << std::endl <<
+  //			std::endl << eig.eigenvectors().col(2) << std::endl;
 
-	//std::cout << "    eigenvectors: " << std::endl << eig.eigenvectors().col(0) <<
-	//			std::endl << eig.eigenvectors().col(1) << std::endl <<
-	//			std::endl << eig.eigenvectors().col(2) << std::endl;
+  //std::cout << "    eigenvalues: " << std::endl << eig.eigenvalues().transpose() << std::endl;
 
-	//std::cout << "    eigenvalues: " << std::endl << eig.eigenvalues().transpose() << std::endl;
+  axis_1 = cv::Point3f(eig.eigenvectors()(0,2), eig.eigenvectors()(1,2), eig.eigenvectors()(2,2));
+  axis_2 = cv::Point3f(eig.eigenvectors()(0,1), eig.eigenvectors()(1,1), eig.eigenvectors()(2,1));
+  axis_3 = cv::Point3f(eig.eigenvectors()(0,0), eig.eigenvectors()(1,0), eig.eigenvectors()(2,0));
 
-	axis_1 = cv::Point3f(eig.eigenvectors()(0,2), eig.eigenvectors()(1,2), eig.eigenvectors()(2,2));
-	axis_2 = cv::Point3f(eig.eigenvectors()(0,1), eig.eigenvectors()(1,1), eig.eigenvectors()(2,1));
-	axis_3 = cv::Point3f(eig.eigenvectors()(0,0), eig.eigenvectors()(1,0), eig.eigenvectors()(2,0));
+  //Multiply axis for the corresponding standart deviation
+  this->standartDeviations.push_back( sqrt(eig.eigenvalues()(0))*2 );
+  this->standartDeviations.push_back( sqrt(eig.eigenvalues()(1))*2 );
+  this->standartDeviations.push_back( sqrt(eig.eigenvalues()(2))*2 );
 
-	//Multiply axis for the corresponding standart deviation
-	this->standartDeviations.push_back( sqrt(eig.eigenvalues()(0))*2 );
-	this->standartDeviations.push_back( sqrt(eig.eigenvalues()(1))*2 );
-	this->standartDeviations.push_back( sqrt(eig.eigenvalues()(2))*2 );
+  this->principalAxis.push_back(axis_1* sqrt( eig.eigenvalues()(2) )*2.0);
+  this->principalAxis.push_back(axis_2* sqrt( eig.eigenvalues()(1) )*2.0);
+  this->principalAxis.push_back(axis_3* sqrt( eig.eigenvalues()(0) )*2.0);
 
-	this->principalAxis.push_back(axis_1* sqrt( eig.eigenvalues()(2) )*2.0);
-	this->principalAxis.push_back(axis_2* sqrt( eig.eigenvalues()(1) )*2.0);
-	this->principalAxis.push_back(axis_3* sqrt( eig.eigenvalues()(0) )*2.0);
-
-	return true;
+  return true;
 
 }
