6f0a421d94175e667ea2c884c8fec8f8409fe293
edgarVazquez <edgarvazquez1403@gmail.com>
2018-02-06 23:48:48 -0600

    Add variance calculator.

--- ROS/catkin_ws/src/test/object_features_test/src/variance_calc.cpp
+++ ROS/catkin_ws/src/test/object_features_test/src/variance_calc.cpp
@@ -15,39 +15,71 @@ int main(int argc, char** argv)
 
     //std::fstream openFile("/home/edgar/ws_points.txt", std::ios_base::in);
     std::fstream openFile("/home/edgar/github/tesis/resultados/objetos/Alturas/height_cerealBox.txt", std::ios_base::in);
+
+    int i=0;
     
     float x, h_edd, h_yisus;
-    std::vector<float> eddList, yisusList;
-    int i=0;
-
-
-    markerSetup();
+    float h_eddMedian, h_yisusMedian;
+    float errorRel_edd, errorRel_yisus;
     
-    ros::Rate loop(80);
+    double var_edd, var_yisus;
+    std::vector<float> eddList, yisusList;
+    std::vector<float> ideal;
 
+    ideal.resize(5);
+
+    ideal[0] = 0.27;  // Cereal box
+    ideal[1] = 0.05;  // Joystick
+    ideal[2] = 0.15;  // Juice
+    ideal[3] = 0.11;  // Milk
+    ideal[4] = 0.03;  // Chocolate
+
+    h_eddMedian = 0;
+    h_yisusMedian = 0;
+    
     for(int i = 0; i < 100; i++)
     {
       std::string line;
       std::getline(openFile, line);   
       std::istringstream in(line);      //make a stream for the line itself
-      in >> x >> h_edd >> h_yisus;      //now read the whitespace-separated floats
+      in >> h_edd >> h_yisus;      //now read the whitespace-separated floats
+
+      h_eddMedian += h_edd;
+      h_yisusMedian += h_yisus;
       
       eddList.push_back(h_edd);
-      yisusList.push_back(h_yisus);
-      
+      yisusList.push_back(h_yisus);  
     }
-    
 
-    while(ros::ok())
+    h_eddMedian /= 100;
+    h_yisusMedian /= 100;
+
+    std::cout << "Median: " << std::endl
+	      << h_eddMedian << "  "
+	      << h_yisusMedian << std::endl;
+
+    for(int i = 0; i < eddList.size(); i++)
     {
-        
-	
-        ros::spinOnce();
-        loop.sleep();
-        i++;
+      var_edd += (eddList[i]-h_eddMedian)*(eddList[i]-h_eddMedian);
+      var_yisus += (yisusList[i]-h_yisusMedian)*(yisusList[i]-h_yisusMedian);
 
-        if(i>100)
-            return 0;
+      errorRel_edd += fabs(eddList[i]-ideal[0])/ideal[0];
+      errorRel_yisus += fabs(yisusList[i]-ideal[0])/ideal[0];
     }
+
+    var_edd /= 99;
+    var_yisus /= 99;
+
+    errorRel_edd /= 99;
+    errorRel_yisus /= 99;
+
+    std::cout << "Var: " << std::endl
+	      << var_edd << "  "
+	      << var_yisus << std::endl;
+
+    std::cout << "Error relativo promedio: " << std::endl
+	      << errorRel_edd << "  "
+	      << errorRel_yisus << std::endl;
+    
     return 0;
 }
